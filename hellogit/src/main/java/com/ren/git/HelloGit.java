1c9adb2d90a28665a6d1f8fcdfd80f3a1687b5ab
ren <ren@example.com>
2021-11-02 20:42:00 +0800

    添加文件

---
+++ hellogit/src/main/java/com/ren/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.ren.git;
+
+/**
+ * @author ren
+ * @create 2021-10-31-15:45
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
